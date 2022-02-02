adaefa9b630165c570f564743d3d2dadcdef511c
Vishal <vishal.byahatti@gmail.com>
2022-02-02 23:48:21 +0530

    Check empty string

--- src/RemoveWordsInBracket.java
+++ src/RemoveWordsInBracket.java
@@ -1,18 +1,17 @@
+import static java.lang.System.out;
+
 public class RemoveWordsInBracket {
 
     public static void main(String[] args) {
         String sentence = "[This] Program [is] [written] for removing words [in] Brackets";
-        System.out.println("Sentence before removing brackets =" + sentence);
+        out.println("Sentence before removing brackets =" + sentence);
         String[] words = sentence.split(" ");
         String newWords = "";
-        for(int i=0;i<words.length;i++){
-            if(! words[i].contains("[") && ! words[i].contains("]")){
-                if(newWords == null)
-                    newWords = words[i];
-                else
-                newWords = newWords + " " + String.valueOf(words[i]);
-            }
-        }
-        System.out.println("Sentence after removing brackets =" + newWords);
+        for (String word : words)
+            if (!word.contains("[") && !word.contains("]")) if (newWords == "")
+                newWords = word;
+            else
+                newWords = newWords + " " + word;
+        out.println("Sentence after removing brackets =" + newWords);
     }
 }
